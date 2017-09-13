5a103061ea0208e9a3b9fa4e79e4f568267588aa
Yuka Suzuki <suzukiyuka1218@gmail.com>
2017-09-14 08:55:49 +0900

    first commit

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerScript : MonoBehaviour
+{
+	int score = 0;
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.Space)) {
+			GetComponent<Rigidbody> ().velocity = new Vector3 (0, 6, 0);
+			GetComponent<AudioSource> ().Play ();
+		}
+	}
+
+	void OnCollisionEnter (Collision other)
+	{
+		Debug.Log ("Gameover");
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		score++;
+		Debug.Log ("スコアは" + score);
+	}
+
+	//tab2回で補完
+
+}
---
+++ Assets/WallCreateScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCreateScript : MonoBehaviour {
+	public GameObject wall;
+	float timer = 0;
+	int interval = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timer += Time.deltaTime;
+		if (timer >= interval) {
+			transform.position = new Vector3 (0, Random.Range (-1.0f, 2.0f), 6);
+			Instantiate (wall, transform.position, transform.rotation);
+			timer = 0;
+		}
+	}
+}
